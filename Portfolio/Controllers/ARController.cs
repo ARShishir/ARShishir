41c5245b624d482722e47b5c2f8ed76c30834d56
ARShishir <shishir01022003@gmail.com>
2024-11-18 10:59:00 +0600

    My protfolio

---
+++ Portfolio/Controllers/ARController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
